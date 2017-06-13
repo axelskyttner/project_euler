34ee0ef6082652e6e19d4b06a540c37afc06a800
Axel <axelskyttner@gmail.com>
2017-06-13 21:36:19 +0200

    added example files for tomorrow

---
+++ js/task_2.js
@@ -0,0 +1,19 @@
+console.log("hello");
+
+var numberArr= [];
+const nr = 100;
+for(var i = 0; i < nr;i++){
+	numberArr.push(NaN);
+
+}
+
+console.log("array", numberArr.reduce);
+console.log("filter", numberArr.filter);
+
+
+function call(){
+	numberArr.reduce()
+
+}
+
+call();
